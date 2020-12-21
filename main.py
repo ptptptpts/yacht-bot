42b7ff275b8e2d5227a1594227b89737e0cbece1
ptptptpts <ptptptpts@gmail.com>
2020-12-21 22:14:35 +0900

    fix: Disable menu and execute AiBot
    Signed-off-by: ptptptpts <ptptptpts@gmail.com>

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from Player.PlayerOne import YachtGamePlayerOne
 from Player.RandomBot import YachtRandomBot
 
 
-print("1. Play Self")
-print("2. Play Random Bot")
-print("3. Play AI Bot")
-select = int(input(":"))
-if select == 1:
-    YachtGamePlayerOne().start()
-elif select == 2:
-    YachtRandomBot().start(100)
-elif select == 3:
-    YachtAiBot().start()
+# print("1. Play Self")
+# print("2. Play Random Bot")
+# print("3. Play AI Bot")
+# select = int(input(":"))
+# if select == 1:
+#     YachtGamePlayerOne().start()
+# elif select == 2:
+#     YachtRandomBot().start(100)
+# elif select == 3:
+#     YachtAiBot().start()
+YachtAiBot().start()
